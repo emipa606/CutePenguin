f525e5be084719b10a2e5cf17a5c27a408bc0cb4
emipa606 <emil.offermo@litet.net>
2020-10-26 22:35:08 +0100

    First publish

---
+++ Source/Cutepenguin/Penguin_JobGiver_Cleaning.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Penguin
+{
+	// Token: 0x02000002 RID: 2
+	public class Penguin_JobGiver_Cleaning : ThinkNode_JobGiver
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		protected override Job TryGiveJob(Pawn pawn)
+		{
+            bool validator(Thing t) => t.def.category == ThingCategory.Projectile;
+            Thing thing = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map, ThingRequest.ForGroup(ThingRequestGroup.Filth), PathEndMode.ClosestTouch, TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false), 300f, validator, null, 0, -1, false, RegionType.Set_Passable, false);
+			if (thing != null && pawn.CanReserve(thing, 1, -1, null, false))
+			{
+				Job job = new Job(JobDefOf.Clean);
+				job.AddQueuedTarget(TargetIndex.A, thing);
+				return job;
+			}
+			return null;
+		}
+	}
+}
---
+++ Source/Cutepenguin/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("Cutepenguin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cutepenguin")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dacfceff-8bc9-4f5e-80fc-d08bdaf9ef6c")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
