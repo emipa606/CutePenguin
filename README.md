56a60889b5b76c686e730a1d93275c57cbbc5358
Mlie <emipa606@users.noreply.github.com>
2022-10-15 20:06:01 +0200

    Mod updated for 1.4 and passed autotests

--- README.md
+++ README.md
@@ -1,35 +1,26 @@
 # CutePenguin
 
-![Image](https://i.imgur.com/WAEzk68.png)
+![Image](https://i.imgur.com/buuPQel.png)
 
 Update of totobrother and Mehnis mod
 https://steamcommunity.com/sharedfiles/filedetails/?id=1264959770
 
-![Image](https://i.imgur.com/7Gzt3Rg.png)
+![Image](https://i.imgur.com/pufA0kM.png)
 
-
-[table]
-    [tr]
-        [td]https://invite.gg/Mlie]![Image](https://i.imgur.com/zdzzBrc.png)
-[/td]
-        [td]https://github.com/emipa606/CutePenguin]![Image](https://i.imgur.com/kTkpTOE.png)
-[/td]
-    [/tr]
-[/table]
 	
-![Image](https://i.imgur.com/NOW7jU1.png)
+![Image](https://i.imgur.com/Z4GOv8H.png)
 
 Add new cute animal [Penguin] for game.
 This penguins are modified gene by Mehni. So They have incredible intelligence!
 They can haul your items and clean your base!
-Don&apos;t treat them harsh. They are useful!
+Don't treat them harsh. They are useful!
 
 Code : Mehni
 XML : totobrother
 Art : totobrother
 Preview image : Allet
 
-![Image](https://i.imgur.com/Rs6T6cr.png)
+![Image](https://i.imgur.com/PwoNOj4.png)
 
 
 
@@ -39,6 +30,7 @@ Preview image : Allet
 -  For best support, please use the Discord-channel for error-reporting.
 -  Do not report errors by making a discussion-thread, I get no notification of that.
 -  If you have the solution for a problem, please post it to the GitHub repository.
+
 
 
 
--- Source/Cutepenguin/Penguin_JobGiver_Cleaning.cs
+++ Source/Cutepenguin/Penguin_JobGiver_Cleaning.cs
@@ -1,31 +1,28 @@
-﻿using RimWorld;
+using RimWorld;
 using Verse;
 using Verse.AI;
 
-namespace Penguin
+namespace Penguin;
+
+public class Penguin_JobGiver_Cleaning : ThinkNode_JobGiver
 {
-    // Token: 0x02000002 RID: 2
-    public class Penguin_JobGiver_Cleaning : ThinkNode_JobGiver
+    protected override Job TryGiveJob(Pawn pawn)
     {
-        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-        protected override Job TryGiveJob(Pawn pawn)
+        bool validator(Thing t)
         {
-            bool validator(Thing t)
-            {
-                return t.def.category == ThingCategory.Projectile;
-            }
-
-            var thing = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map,
-                ThingRequest.ForGroup(ThingRequestGroup.Filth), PathEndMode.ClosestTouch, TraverseParms.For(pawn), 300f,
-                validator);
-            if (thing == null || !pawn.CanReserve(thing))
-            {
-                return null;
-            }
-
-            var job = new Job(JobDefOf.Clean);
-            job.AddQueuedTarget(TargetIndex.A, thing);
-            return job;
+            return t.def.category == ThingCategory.Projectile;
         }
+
+        var thing = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map,
+            ThingRequest.ForGroup(ThingRequestGroup.Filth), PathEndMode.ClosestTouch, TraverseParms.For(pawn), 300f,
+            validator);
+        if (thing == null || !pawn.CanReserve(thing))
+        {
+            return null;
+        }
+
+        var job = new Job(JobDefOf.Clean);
+        job.AddQueuedTarget(TargetIndex.A, thing);
+        return job;
     }
 }
