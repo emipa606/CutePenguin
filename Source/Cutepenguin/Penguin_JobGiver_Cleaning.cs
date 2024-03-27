4be9b94915b73593a08f5d7fa73b8f925deb0d2f
Mlie <emipa606@users.noreply.github.com>
2024-03-27 22:26:57 +0100

    Mod updated for 1.5 and passed autotests

--- Source/Cutepenguin/Penguin_JobGiver_Cleaning.cs
+++ Source/Cutepenguin/Penguin_JobGiver_Cleaning.cs
@@ -8,11 +8,6 @@ public class Penguin_JobGiver_Cleaning : ThinkNode_JobGiver
 {
     protected override Job TryGiveJob(Pawn pawn)
     {
-        bool validator(Thing t)
-        {
-            return t.def.category == ThingCategory.Projectile;
-        }
-
         var thing = GenClosest.ClosestThingReachable(pawn.Position, pawn.Map,
             ThingRequest.ForGroup(ThingRequestGroup.Filth), PathEndMode.ClosestTouch, TraverseParms.For(pawn), 300f,
             validator);
@@ -24,5 +19,10 @@ public class Penguin_JobGiver_Cleaning : ThinkNode_JobGiver
         var job = new Job(JobDefOf.Clean);
         job.AddQueuedTarget(TargetIndex.A, thing);
         return job;
+
+        bool validator(Thing t)
+        {
+            return t.def.category == ThingCategory.Projectile;
+        }
     }
 }
